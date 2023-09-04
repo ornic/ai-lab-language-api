41bc8d179b59b3ee2ab721689fafe23af13db434
Roman Orlov <orlov@tiburon-research.ru>
2023-09-04 16:54:35 +0300

    экспериментируем с glc3

--- server.py
+++ server.py
@@ -2,45 +2,87 @@ from aiohttp import web
 import multiprocessing
 import fasttext
 import json
-import jsonpickle
+import gcld3
 
 model = None
+detector = None
+
+
+def isModelLoaded():
+    return not ((model is None) or (detector is None))
+
 
 def loadModel():
     global model
     model = fasttext.load_model("./models/lid.176.bin")
-    
+    detector = gcld3.NNetLanguageIdentifier(min_num_bytes=0, max_num_bytes=1000)
+
+
+async def healthcheck(request):
+    try:
+        if not isModelLoaded():
+            loadModel()
+
+        return web.Response(text="pong")
+
+    except Exception as e:
+        return web.Response(text="Error type: " + str(type(e)), status=500)
+
+
 async def about(request):
-    text = "Server: Gunicorn. CPU count: " + str(multiprocessing.cpu_count())
-    text = text + "\n\n" + "Example:"
-    text = text + "\n\n" + "post('http://url/language', params='', data=json.dumps({'text': 'Nynorsk og bokmål er jamstilte målformer i Noreg, og det har dei to skriftspråka vore sidan 1885.'}), headers={'content-type': 'application/json'})"
+    if not isModelLoaded():
+        loadModel()
+
+    text = (
+        "Server: Gunicorn. CPU count: "
+        + str(multiprocessing.cpu_count())
+        + ". FastText model words count: "
+        + str(len(model.words))
+        + "."
+    )
     return web.Response(text=text)
 
 
 async def post_language(request):
     try:
-
-        if model is None:      
+        if not isModelLoaded():
             loadModel()
 
         data = await request.json()
-        text = data['text']
-        output = model.predict(text,1)
-        language = output[0][0].replace('__label__','')
-        res = {"language": language, "original_response": jsonpickle.encode(output)}
+        text = data["text"]
+
+        print("Got text to detect language: [" + text + "]", flush=True)
+
+        output = model.predict(text, 1)
+        language = output[0][0].replace("__label__", "")
+        print("Fasttext result: [" + language + "]", flush=True)
+
+        result = detector.FindLanguage(text=text)
+        print("CLD3 result: [" + result.language + "]", flush=True)
+
+        if result.language != language:
+            raise ValueError(
+                "Fasttext language detected: "
+                + language
+                + ", but cld3 found: "
+                + result.language
+            )
+
+        res = {"language": language}
     except Exception as e:
-        print("Exception!")
-        print(e)
-        res = {"language": "n/a", "error": json.dumps(e.__dict__, ensure_ascii=False)}
+        print("Error:", flush=True)
+        print(e, flush=True)
+        res = {"language": "n/a", "error": json.dumps(e.__dict__)}
     return web.json_response(res)
 
 
 app = web.Application()
-app.router.add_get('/', about)
-app.router.add_post('/language', post_language)
+app.router.add_get("/", about)
+app.router.add_get("/ping", healthcheck)
+app.router.add_post("/language", post_language)
 
+print("starting [" + __name__ + "]", flush=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     loadModel()
-    web.run_app(app, port=8080)
-
+    web.run_app(app, port=8000)
