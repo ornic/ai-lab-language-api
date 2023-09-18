b1c77af1b61f506484b6877e2e2e9a4db242d5d4
Roman Orlov <orlov@tiburon-research.ru>
2023-09-18 17:56:35 +0300

    пишем греложные фильтры ещё лучше, только они всё равно не работают

--- graylog.py
+++ graylog.py
@@ -11,11 +11,20 @@ class AddtitionalFieldsFilter(logging.Filter):
     def __init__(self):
         # In an actual use case would dynamically get this
         # (e.g. from memcache)
-        self.environment = os.environ[PYTHON_ENVIRONMENT_NAME]
+        if PYTHON_ENVIRONMENT_NAME in os.environ:
+            self.environment = os.environ[PYTHON_ENVIRONMENT_NAME]
+
+        self.solution = "FastunaAI"
+        self.source = "Language.Detection"
 
     def filter(self, record):
-        record.environment = os.environ[PYTHON_ENVIRONMENT_NAME]
-        return PYTHON_ENVIRONMENT_NAME in os.environ
+        if PYTHON_ENVIRONMENT_NAME in os.environ:
+            record.environment = os.environ[PYTHON_ENVIRONMENT_NAME]
+
+        record.solution = "FastunaAI"
+        record.source = "Language.Detection"
+
+        return True
 
 
 graylog_logger = logging.getLogger("language_detector_logger")
@@ -25,23 +34,23 @@ graylog_logger.addFilter(AddtitionalFieldsFilter())
 
 
 def make_extra(payload: str):
-    return {"solution": "FastunaAI", "source": "Language.Detection", "payload": payload}
+    return {"payload": payload}
 
 
 def netlog_error(message: str, need_trace: bool):
     try:
+        graylog_logger.critical(message)
         if need_trace:
             logger.exception(message)
         else:
             logger.error(message)
-        graylog_logger.critical(message)
     except Exception as e:
-        print(e)
+        logger.exception("Error sending logs")
 
 
 def netlog_info(message: str, payload: str):
     try:
+        graylog_logger.info(message, extra=make_extra(payload))
         logger.info(message)
-        graylog_logger.debug(message, extra=make_extra(payload))
     except Exception as e:
-        print(e)
+        logger.exception("Error sending logs")
